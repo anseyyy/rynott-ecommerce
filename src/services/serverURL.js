b19934294003ccabb0963b5515ec358f055e09d4
anseyyy <ahamedansil43@gmail.com>
2025-12-02 12:08:47 +0530

    update

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -4,7 +4,7 @@ const serverUrl =
   (typeof process !== "undefined" &&
     process.env &&
     (process.env.VITE_API_URL || process.env.REACT_APP_API_URL)) ||
-  "https://rynott-ecommerce-server.onrender.com/api/";
+  "https://rynott-ecommerce-server.onrender.com";
 
 export const apiBaseUrl = `${serverUrl}/api`;
 
